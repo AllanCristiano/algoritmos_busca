fff1c77d0fa4974f4d9d616b6c6b8fed22841d24
AllanCristiano <drmundo920@gmail.com>
2024-05-01 18:44:08 -0300

    implementação da busca Gulosa

--- b_gulosa/insere_vetor_gulosa.py
+++ b_gulosa/insere_vetor_gulosa.py
@@ -1,34 +1,35 @@
 import numpy as np
 
+
 class OrdenaVetor:
     def __init__(self, capacidade) -> None:
         self.capacidade = capacidade
         self.final = -1
-        
-        self.valores = np.empty(capacidade, dtype=object)
-        
-    def insere(self, adjacente) -> None:
-        if self.final == self.capacidade -1:
-            print("Capacidade maxima")
+
+        self.valores = np.empty(self.capacidade, dtype=object)
+
+    def insere(self, vertice) -> None:
+
+        if self.final== self.capacidade - 1:
+            print('Capacidade máxima atingida')
             return
-        
-        posicao = 0         
-        for i in range(self.capacide + 1):
+        posicao = 0
+        for i in range(self.final + 1):
             posicao = i
-            if posicao == self.final:
+            if self.valores[i].distancia_objetivo > vertice.distancia_objetivo:
+                break
+            if i == self.final:
                 posicao = i + 1
-        
         x = self.final
         while x >= posicao:
             self.valores[x + 1] = self.valores[x]
             x -= 1
-        
-        self.valores[posicao] = adjacente
+        self.valores[posicao] = vertice
         self.final += 1
-        
+
     def imprime(self):
-        if self.ultima_posicao == -1:
+        if self.final == -1:
             print('O vetor está vazio')
         else:
-            for i in range(self.ultima_posicao + 1):
+            for i in range(self.final + 1):
                 print(i, ' - ', self.valores[i].rotulo, ' - ', self.valores[i].distancia_objetivo)
--- b_gulosa/main.py
+++ b_gulosa/main.py
@@ -2,6 +2,29 @@ from grafo import Grafo
 from insere_vetor_gulosa import OrdenaVetor
 
 
+class BuscaGulosa:
+    def __init__(self, obejetivo) -> None:
+        self.objetivo = obejetivo
+        self.encontrado = False
+
+    def buscar(self, atual):
+        print("----------------")
+        print(f'Atual: {atual.rotulo}')
+        atual.visitado = True
+
+        if atual == self.objetivo:
+            self.encontrado = True
+        else:
+            vetor = OrdenaVetor(len(atual.adjacencias))
+            for i in atual.adjacencias:
+                if not i.vertice.visitado:
+                    i.vertice.visitado = True
+                    vetor.insere(i.vertice)
+            vetor.imprime()
+            if vetor.valores[0] is not None:
+                self.buscar(vetor.valores[0])
+
+
 grafo = Grafo()
-
-
+buscaGulosa = BuscaGulosa(grafo.curitiba)
+buscaGulosa.buscar(grafo.porto_uniao)
